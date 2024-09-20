32dbfa23cb8fec35b4e9944eb8d8f57f2fd53695
nowrin <nowrinnova0088@gmail.com>
2024-09-21 00:45:50 +0600

    add money pin verify is done

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,8 +3,13 @@ document.getElementById("addMoney-btn").addEventListener("click", (event) => {
   const amountValue = getInputFieldValueById("amount-field");
   console.log(`amount ${amountValue}`);
   const pinNumber = getInputFieldValueById("addNumber-pin");
-  console.log(`pin ${pinNumber}`);
+  if(pinNumber==1234){
   const accountBalance = amountBalance("account-balance");
   const updateAccountBalance=accountBalance+amountValue;
   document.getElementById('account-balance').innerText=updateAccountBalance;
+  }
+  else{
+    alert('wrong input.please fill the valid input');
+  }
+  
 });
