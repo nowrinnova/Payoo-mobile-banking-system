b1d3016f259f19e2e85e468a0bd3aca0b7658af3
nowrin <nowrinnova0088@gmail.com>
2024-09-21 00:54:10 +0600

    after giving pin and amount field will be vanish

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,12 +1,14 @@
 document.getElementById("addMoney-btn").addEventListener("click", (event) => {
   event.preventDefault();
   const amountValue = getInputFieldValueById("amount-field");
+  document.getElementById('amount-field').value="";
   console.log(`amount ${amountValue}`);
   const pinNumber = getInputFieldValueById("addNumber-pin");
   if(pinNumber==1234){
   const accountBalance = amountBalance("account-balance");
   const updateAccountBalance=accountBalance+amountValue;
   document.getElementById('account-balance').innerText=updateAccountBalance;
+  document.getElementById('addNumber-pin').value="";
   }
   else{
     alert('wrong input.please fill the valid input');
